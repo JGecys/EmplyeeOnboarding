3f9fe194385362ca06e1b6a1fabce62b7526d609
JurgisG <jgecys@gmail.com>
2016-03-06 14:35:16 +0200

    cleanup

--- src/main/java/com/hackergames/emplyeeonboarding/GmailService.java
+++ src/main/java/com/hackergames/emplyeeonboarding/GmailService.java
@@ -42,9 +42,6 @@ import java.util.*;
 public class GmailService {
 
     @Autowired
-    OAuth2RestTemplate template;
-
-    @Autowired
     OAuth2ClientContext context;
 
     /**
@@ -72,10 +69,18 @@ public class GmailService {
         return new Gmail.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential).setApplicationName("EmplyeeOnboarding").build();
     }
 
+    /**
+     * @return list account's labels
+     * @throws IOException
+     */
     public List<Label> getLabels() throws IOException {
         return service().users().labels().list("me").execute().getLabels();
     }
 
+    /**
+     * @return list of all emails in your inbox.
+     * @throws IOException
+     */
     public List<Message> getMessages() throws IOException { // TODO: 2016-03-05  make it async
         String user = "me";
         Gmail service = service();
@@ -99,6 +104,14 @@ public class GmailService {
         return messages;
     }
 
+    /**
+     * Get and decode email
+     *
+     * @param messageId email id
+     * @return  MimeMessage of email.
+     * @throws IOException
+     * @throws MessagingException
+     */
     public MimeMessage getMessage(String messageId) throws IOException, MessagingException {
         Message msg = service().users().messages().get("me", messageId).setFormat("raw").execute();
         byte[] emailBytes = Base64.decodeBase64(msg.getRaw());
@@ -108,6 +121,14 @@ public class GmailService {
     }
 
 
+    /**
+     * !Doesnt work!
+     *
+     * @param messageId
+     * @return
+     * @throws IOException
+     * @throws MessagingException
+     */
     public String getMessageCategory(String messageId) throws IOException, MessagingException {
         MimeMessage message = getMessage(messageId);
         InputStream in = new FileInputStream("/training/modelFile.train");
@@ -122,24 +143,13 @@ public class GmailService {
 
     public String getMessageContent(String messageId) throws IOException, MessagingException {
         MimeMessage message = getMessage(messageId);
-//        String msg = IOUtils.toString(message.getInputStream(), "UTF-8");
-//        EmailUtils.extractClearText(m, null);
         String msg = getClearText(message);
-//        if(message.getContentType().equals("text/html")){
-//            return Jsoup.parse(msg).body().text();
-//        }
-//        if(message.getContentType().equals("text/plain")){
-//            return msg;
-//        }
-//        Document document = Jsoup.parse(msg);
-//        document.select("a").remove();
-//        document.body().text();
         msg = msg.replaceAll("https?://\\S+\\s?", "");
         return msg;
     }
 
     private String getClearText(MimeMessage message) throws MessagingException, IOException {
-        MimeMessage m = (MimeMessage) message;
+        MimeMessage m = message;
         Object contentObject = m.getContent();
         StringBuilder result = new StringBuilder();
         if (contentObject instanceof Multipart) {
@@ -196,47 +206,6 @@ public class GmailService {
             keywords.add(e.getKey());
         }
         return keywords;
-    }
-
-    public void train(String messageId) throws IOException, MessagingException {
-
-        MimeMessage message = getMessage(messageId);
-        String modelFilePath = "/training/modelFile.train";
-        // Instance of openNLP's default model class
-        DoccatModel model = null;
-        InputStream dataIn = null;
-        try {
-            dataIn = message.getInputStream();
-            ObjectStream<String> lineStream = new PlainTextByLineStream(dataIn, "UTF-8");
-            ObjectStream<DocumentSample> sampleStream = new DocumentSampleStream(lineStream);
-            // "en" is language code of English.
-            model = DocumentCategorizerME.train("en", sampleStream);
-        } catch (IOException e) {
-            System.out.println("Failed to read or parse training data, training failed");
-        } finally {
-            if (dataIn != null) {
-                try {
-                    dataIn.close();
-                } catch (IOException e) {
-                    System.out.println(e.getLocalizedMessage());
-                }
-            }
-        }
-        OutputStream modelOut = null;
-        try {
-            modelOut = new BufferedOutputStream(new FileOutputStream(modelFilePath));
-            model.serialize(modelOut);
-        } catch (IOException e) {
-            System.out.println("Failed to save model at location " + modelFilePath);
-        } finally {
-            if (modelOut != null) {
-                try {
-                    modelOut.close();
-                } catch (IOException e) {
-                    System.out.println("Failed to correctly save model. Written model might be invalid.");
-                }
-            }
-        }
     }
 
 
